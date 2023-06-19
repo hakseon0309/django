25588ea2d730f1dd987b84fb488ef7fbc5889893
hakseon0309 <hakseon0309@gmail.com>
2023-06-19 21:23:01 +0900

    0619backup

--- app1/views.py
+++ app1/views.py
@@ -7,13 +7,18 @@ rooms = [
     {"id": 3, "name": "Frontend developer"},
 ]
 
-profile = [{"name": 1, "server": "KR-Azshara"}]
+profile = [{"name": "학선양", "server": "KR-Azshara"}]
 
 
-def main(request):
+def home(request):
     context = {"profile": profile}
     return render(request, "app1/home.html", context)
 
 
 def content(request, pk):
+    content = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
     return render(request, "app1/content.html")
