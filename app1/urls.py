550eb722d8e1c3f62d558b8e7dfc1e92a54faa63
hakseon0309 <hakseon0309@gmail.com>
2023-06-23 14:55:45 +0900

    backup

--- app1/urls.py
+++ app1/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("register/", views.registerPage, name="register"),
     path("", views.home, name="home"),
     path("home/", views.home, name="home1"),
-    # pk = primary key
     path("post/<str:pk>/", views.post, name="post"),
+    path("create-post/", views.createPost, name="create-post"),
+    path("update-post/<str:pk>", views.updatePost, name="update-post"),
+    path("delete-post/<str:pk>", views.deletePost, name="delete-post"),
+    path("delete-message/<str:pk>", views.deleteMessage, name="delete-message"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,24 +1,147 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Post, Topic, Message
+from .forms import PostForm
 
 
-posts = [
-    {"id": 1, "title": "number1 post", "desc": "content1"},
-    {"id": 2, "title": "number2 post", "desc": "content2"},
-    {"id": 3, "title": "number3 post", "desc": "content3"},
-]
+def loginPage(request):
+    page = "login"
+    if request.user.is_authenticated:
+        return redirect("home")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-profile = [{"name": "학선양", "server": "KR-Azshara"}]
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "사용자를 찾을 수 없습니다.")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "아이디나 비밀번호가 올바르지 않습니다.")
+
+    context = {"page": page}
+    return render(request, "app1/login_register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "계정을 만드는 과정에서 문제가 발생했습니다.")
+
+    return render(request, "app1/login_register.html", {"form": form})
 
 
 def home(request):
-    context = {"posts": posts}
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    posts = Post.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+    post_count = posts.count()
+
+    post_messages = Message.objects.filter(Q(post__topic__name__icontains=q))
+
+    context = {
+        "posts": posts,
+        "topics": topics,
+        "post_count": post_count,
+        "post_messages": post_messages,
+    }
     return render(request, "app1/home.html", context)
 
 
 def post(request, pk):
-    post = None
-    for i in posts:
-        if i["id"] == int(pk):
-            post = i
-    context = {"post": post}
+    post = Post.objects.get(id=pk)
+    post_messages = post.message_set.all().order_by("-created")
+    participants = post.participants.all()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user=request.user, post=post, body=request.POST.get("body")
+        )
+        post.participants.add(request.user)
+        return redirect("post", pk=post.id)
+
+    context = {
+        "post": post,
+        "post_messages": post_messages,
+        "participants": participants,
+    }
     return render(request, "app1/post.html", context)
+
+
+@login_required(login_url="login")
+def createPost(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "app1/post_form.html", context)
+
+
+@login_required(login_url="login")
+def updatePost(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+
+    if request.user != post.host:
+        return HttpResponse("당신은 이 글을 수정할 수 없습니다")
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "app1/post_form.html", context)
+
+
+@login_required(login_url="login")
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.user != post.host:
+        return HttpResponse("당신은 이 글을 수정할 수 없습니다")
+    if request.method == "POST":
+        post.delete()
+        return redirect("home")
+    return render(request, "app1/delete.html", {"obj": post})
+
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != messages.user:
+        return HttpResponse("당신은 이 댓글을 수정할 수 없습니다")
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "app1/delete.html", {"obj": message})
